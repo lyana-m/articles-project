65ea66bf52d3384c0df4d2fdc2d622d484353716
lyana <lyana-m@yandex.ru>
2023-04-13 23:46:46 +0500

    add bandle analizer

--- config/build/buildPlagins.ts
+++ config/build/buildPlagins.ts
@@ -1,20 +1,24 @@
-import { ProgressPlugin, type WebpackPluginInstance, DefinePlugin } from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlagin from 'mini-css-extract-plugin'
-import { type BuildOptions } from './types/config'
+import { ProgressPlugin, type WebpackPluginInstance, DefinePlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlagin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { type BuildOptions } from './types/config';
 
 export const buildPlagins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
   return [
     new HtmlWebpackPlugin({
-      template: paths.html
+      template: paths.html,
     }),
     new ProgressPlugin(),
     new MiniCssExtractPlagin({
       filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
-    })
-  ]
-}
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
+  ];
+};
