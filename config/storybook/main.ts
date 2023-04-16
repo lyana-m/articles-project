3d0644c83ac61e6eae3eaf62fd4ec43e18ed136d
lyana <lyana-m@yandex.ru>
2023-04-16 21:56:10 +0500

    add loki

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -18,6 +18,7 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
+  features: { storyStoreV7: false },
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
