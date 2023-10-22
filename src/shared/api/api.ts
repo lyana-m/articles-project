f494da32f053213088c924b8c7eafac2016399f2
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2023-10-22 12:15:18 +0500

    add axios interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,13 @@ import { USER_LOCALSTORAGE_AUTHDATA } from 'shared/constants/localStorage';
 export const api = axios.create({
   baseURL: __API__,
   headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_AUTHDATA),
     'Content-Type': 'application/json',
   },
 });
+
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem(USER_LOCALSTORAGE_AUTHDATA) || ''}`;
+    return config;
+  },
+);
